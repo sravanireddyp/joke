4250f998d05fa2fc7732a9612ebcc6825270dde3
sravanireddyp <shravanireddypasunoori@gmail.com>
2021-09-21 16:21:16 +0530

    joke

---
+++ script.js
@@ -0,0 +1,18 @@
+//assign the variables
+const jokeEl=document.getElementById("joke");
+const jokeBtn=document.getElementById("jokeBtn");
+jokeBtn.addEventListener('click',generateJoke)
+generateJoke();
+
+async function generateJoke()
+{
+    const config =
+    {
+        headers:{
+            Accept:'application/json'
+        }
+    }
+    const response = await fetch ('https://icanhazdadjoke.com/',config);
+    const data=await response.json();
+    jokeEl.innerHTML=data.joke; 
+}
